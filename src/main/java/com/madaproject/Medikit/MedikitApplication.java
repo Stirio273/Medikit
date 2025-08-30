1327a7cf3e0716c483bf054334b8310d7e3180ff
Stirio273 <tsioryitokiana273@gmail.com>
2025-08-30 12:37:56 +0300

    Initial commit

---
+++ src/main/java/com/madaproject/Medikit/MedikitApplication.java
@@ -0,0 +1,13 @@
+package com.madaproject.Medikit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedikitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedikitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/madaproject/Medikit/MedikitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madaproject.Medikit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedikitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
